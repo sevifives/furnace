fa21b2246cf7be389a859f292ae9d1a7dcc218fe
Seth Carney <seth.carney@eloqua.com>
2012-06-07 15:42:59 -0400

    make required prop more robust

--- properties.js
+++ properties.js
@@ -1,8 +1,15 @@
 var property = require('./property');
 
-var requiredField = property({ 
-  required: true
-});
+var requiredField = function(options) {
+  var prop = { required: true };
+
+  if (!options) options = {};
+  Object.keys(options).forEach(function(option) {
+    prop[option] = options[option];
+  });
+
+  return prop;
+};
 
 module.exports = {
   requiredField: requiredField
