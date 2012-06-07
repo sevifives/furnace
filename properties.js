14926726c4dbca94f9d0d30fa5ac8beb98e8a42c
Seth Carney <seth.carney@eloqua.com>
2012-06-07 15:44:10 -0400

    change export format

--- properties.js
+++ properties.js
@@ -1,16 +1,16 @@
 var property = require('./property');
 
-var requiredField = function(options) {
-  var prop = { required: true };
-
-  if (!options) options = {};
-  Object.keys(options).forEach(function(option) {
-    prop[option] = options[option];
-  });
-
-  return prop;
-};
-
 module.exports = {
-  requiredField: requiredField
+
+  requiredField: function(options) {
+    var prop = { required: true };
+
+    if (!options) options = {};
+    Object.keys(options).forEach(function(option) {
+      prop[option] = options[option];
+    });
+
+    return prop;
+  }
+
 };
